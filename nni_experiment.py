a812f6b981e50281416e044f634cf54a7683048b
xu <1242474153@qq.com>
2022-12-16 19:56:49 +0800

    update

--- nni_experiment.py
+++ nni_experiment.py
@@ -6,14 +6,14 @@ search_space = {
     "local_weight":{"_type":"loguniform","_value":[0.0001,0.1]},
     "lr":{"_type":"loguniform","_value":[0.00001,0.0015]},
     "meta_lr":{"_type":"loguniform","_value":[0.01,0.15]},
-    # 'repr_dims':{"_type":"choice", "_value": [128,]}, # 32,64, 256,320
-    # 'max_train_length':{"_type":"choice", "_value": [256,512,1024,1280,1536,2048]}, #
-    # 'depth':{"_type":"choice", "_value": [10]},        # 3,5,7,9,
-    # 'dropout':{"_type":"choice", "_value": [0.1]},    # 0.1,0.6, ,0.2,0.3,0.4,0.5,0.7
+    'repr_dims':{"_type":"choice", "_value": [320,]}, # 32,64, 256,320
+    'max_train_length':{"_type":"choice", "_value": [2048]}, # 256,512,1024,1280,1536,
+    'depth':{"_type":"choice", "_value": [10]},        # 3,5,7,9,
+    'dropout':{"_type":"choice", "_value": [0.1]},    # 0.1,0.6, ,0.2,0.3,0.4,0.5,0.7
     # "dropout":{"_type":"uniform","_value":[0.05,0.45]}, # a*10  ,
-    # 'hidden_dims':{"_type":"choice", "_value": [64,]}, # 8,16,32,128
+    'hidden_dims':{"_type":"choice", "_value": [64,]}, # 8,16,32,128
     # 'ratio_step':{"_type":"choice", "_value": [1]},
-    # 'mask_mode':{"_type":"choice", "_value": ['mask_last']}, # ''binomial','all_true','continuous',
+    'mask_mode':{"_type":"choice", "_value": ['mask_last']}, # ''binomial','all_true','continuous',
 }
 experiment = Experiment('local')
 experiment.config.trial_command = 'python3 train_forecasting.py'
@@ -21,7 +21,7 @@ experiment.config.trial_code_directory = '.'
 experiment.config.search_space = search_space
 experiment.config.tuner.name = 'TPE'
 experiment.config.tuner.class_args['optimize_mode'] = 'minimize'
-experiment.config.max_trial_number = 100
+experiment.config.max_trial_number = 1000
 experiment.config.trial_concurrency = 1
 experiment.run(8080)
 input()
